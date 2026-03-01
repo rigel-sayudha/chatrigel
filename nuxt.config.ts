d473f8c87dacd66fdbe1923f2b7e2221df1f8c4b
rigel-sayudha <rigeldonovan@gmail.com>
2026-03-01 15:10:21 +0800

    chore: replace default nuxt favicon with chatrigel bot svg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       ],
     },
   },
